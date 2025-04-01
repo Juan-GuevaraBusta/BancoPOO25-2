94895f83b4951ca1e9194a40986165cad7d7efc1
Juan-GuevaraBusta <jguevarabustamante25@gmail.com>
2025-03-31 22:02:53 -0500

    Actualizacion 1
    Se agregaron los metodos y atributos en ambas clases

--- TareaBanco/Banco.h
+++ TareaBanco/Banco.h
@@ -4,10 +4,34 @@
 
 #ifndef BANCO_H
 #define BANCO_H
-
+#include <string>
+#include <vector>
+using namespace std;
+#include "cuenta.h"
 
 
 class Banco {
+private:
+    string generarNumeroCuenta();
+    bool validaNumeroCuenta(string numeroCuenta);
+    vector <Cuenta> cuentas;
+    public:
+    Banco();
+    Cuenta agregarCuenta(string titular, string telefono, string numeroCuenta, float saldo);
+    Cuenta buscarPorNombre(string nombre);
+    Cuenta buscarPorCuenta(string numeroCuenta);
+    vector <Cuenta> listarCuentas();
+    Cuenta modificarInfoCuenta();
+    string getNumeroCuenta();
+    string getTitularCuenta();
+    string getTelefonoCuenta();
+    float getSaldoCuenta();
+    string setNumeroCuenta(string numeroCuenta);
+    string setTitularCuenta(string titular);
+    string setTelefonoCuenta(string numeroCuenta);
+    float setSaldoCuenta(float saldo);
+    bool existeCuenta(string numeroCuenta);
+
 
 };
 
--- TareaBanco/Cuenta.h
+++ TareaBanco/Cuenta.h
@@ -4,10 +4,31 @@
 
 #ifndef CUENTA_H
 #define CUENTA_H
-
+#include <vector>
+#include <string>
+using namespace std;
 
 
 class Cuenta {
+private:
+    string titular;
+    string telefono;
+    string numeroCuenta;
+    float saldo;
+public:
+    string getTitular();
+    string getTelefono();
+    string getNumeroCuenta();
+    float getSaldo();
+    string setTitular();
+    string setTelefono();
+    string setNumeroCuenta();
+    float setSaldo();
+    Cuenta();
+    ~Cuenta();
+
+
+
 
 };
 
